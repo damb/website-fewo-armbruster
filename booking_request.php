8477292c180b18accf1d59a3a945b8898f1d7544
Daniel Armbruster <dani.armbruster@gmail.com>
2023-05-17 21:15:33 +0200

    Check form fields (name and dates)

--- booking_request.php
+++ booking_request.php
@@ -7,46 +7,63 @@ use PHPMailer\PHPMailer\SMTP;
 
 require './vendor/autoload.php';
 
+function isValidDate($date, $format = 'Y-m-d')
+{
+    $d = DateTime::createFromFormat($format, $date);
+    // The Y ( 4 digits year ) returns TRUE for any integer with any number of
+    // digits so changing the comparison from == to === fixes the issue.
+    return $d && $d->format($format) === $date;
+}
+
 
 $msg = '';
 //Don't run this unless we're handling a form submission
 if (array_key_exists('email', $_POST)) {
   date_default_timezone_set('Etc/UTC');
 
-  //Create a new PHPMailer instance
-  $mail = new PHPMailer();
+  //print_r($_POST);
+  
+  if (!array_key_exists('name', $_POST) or empty($_POST['name'])) {
+    $msg = 'Name not specified, message ignored.';
+  } elseif (!array_key_exists('arrival_date', $_POST) or !isValidDate($_POST['arrival_date'])) {
+    $msg = 'Invalid arrival date specified, message ignored.';
+  } elseif (!array_key_exists('departure_date', $_POST) or !isValidDate($_POST['departure_date'])) {
+    $msg = 'Invalid departure date specified, message ignored.';
+  } else {
+    //Create a new PHPMailer instance
+    $mail = new PHPMailer();
 
-  $mail->isSMTP();
-  //Enable SMTP debugging
-  //SMTP::DEBUG_OFF = off (for production use)
-  //SMTP::DEBUG_CLIENT = client messages
-  //SMTP::DEBUG_SERVER = client and server messages
-  /* $mail->SMTPDebug = SMTP::DEBUG_SERVER; */
-  $mail->SMTPDebug = SMTP::DEBUG_OFF;
-  $mail->Host = 'mail.co-n.de';
-  $mail->Port = 587;
-  $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-  $mail->SMTPAuth = true;
-  $mail->Username   = 'booking@ferienwohnung-armbruster.de';
-  $mail->Password   = '';
-  $mail->CharSet = PHPMailer::CHARSET_UTF8;
+    $mail->isSMTP();
+    //Enable SMTP debugging
+    //SMTP::DEBUG_OFF = off (for production use)
+    //SMTP::DEBUG_CLIENT = client messages
+    //SMTP::DEBUG_SERVER = client and server messages
+    /* $mail->SMTPDebug = SMTP::DEBUG_SERVER; */
+    $mail->SMTPDebug = SMTP::DEBUG_OFF;
+    $mail->Host = 'mail.co-n.de';
+    $mail->Port = 587;
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->SMTPAuth = true;
+    $mail->Username   = 'booking@ferienwohnung-armbruster.de';
+    $mail->Password   = '';
+    $mail->CharSet = PHPMailer::CHARSET_UTF8;
 
-  $mail->setFrom('booking@ferienwohnung-armbruster.de', 'Buchungsanfrage Ferienwohnung');
+    $mail->setFrom('booking@ferienwohnung-armbruster.de', 'Buchungsanfrage Ferienwohnung');
 
-  //Put the submitter's address in a reply-to header
-  //This will fail if the address provided is invalid,
-  //in which case we should ignore the whole request
-  if ($mail->addReplyTo($_POST['email'], $_POST['name'])) {
+    //Put the submitter's address in a reply-to header
+    //This will fail if the address provided is invalid,
+    //in which case we should ignore the whole request
+    if ($mail->addReplyTo($_POST['email'], $_POST['name'])) {
 
-    //Set who the message is to be sent to
-    $mail->addAddress('k-a.armbruster@gmx.de', 'Andrea Armbruster');
+      //Set who the message is to be sent to
+      $mail->addAddress('k-a.armbruster@gmx.de', 'Andrea Armbruster');
 
-    //Set the subject line
-    $mail->Subject = 'Buchungsanfrage Ferienwohnung ('. $_POST['arrival_date'] . ' - ' . $_POST['departure_date'] . ')';
-    $mail->isHTML(false);
+      //Set the subject line
+      $mail->Subject = 'Buchungsanfrage Ferienwohnung ('. $_POST['arrival_date'] . ' - ' . $_POST['departure_date'] . ')';
+      $mail->isHTML(false);
 
 
-    $mail->Body = <<<EOT
+      $mail->Body = <<<EOT
 Buchungsanfrage:
 ----------------
 Name: {$_POST['name']}
@@ -58,19 +75,20 @@ Anzahl Personen: {$_POST['number_of_persons']} (davon Kinder: {$_POST['number_of
 Mitteilung: {$_POST['message']}
 EOT;
 
-    //send the message, check for errors
-    if (!$mail->send()) {
-        $msg = 'Mailer Error: ' . $mail->ErrorInfo;
+      //send the message, check for errors
+      if (!$mail->send()) {
+         $msg = 'Mailer Error: ' . $mail->ErrorInfo;
+      } else {
+         $msg = 'Message sent! Thanks for contacting us.';
+          //Section 2: IMAP
+          //Uncomment these to save your message in the 'Sent Mail' folder.
+          #if (save_mail($mail)) {
+          #    echo "Message saved!";
+          #}
+      }
     } else {
-       $msg = 'Message sent! Thanks for contacting us.';
-        //Section 2: IMAP
-        //Uncomment these to save your message in the 'Sent Mail' folder.
-        #if (save_mail($mail)) {
-        #    echo "Message saved!";
-        #}
+      $msg = 'Invalid email address, message ignored.';
     }
-  } else {
-    $msg = 'Invalid email address, message ignored.';
   }
 }
 
