5128312757aad0f6e9b02376ca158f0d82089a91
Daniel Armbruster <dani.armbruster@gmail.com>
2023-05-17 11:58:59 +0200

    Add slideshow

---
+++ slideshow.js
@@ -0,0 +1,20 @@
+// Slideshow
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("slideshow-slides");
+  let dots = document.getElementsByClassName("slideshow-dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" slideshow-dot-active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " slideshow-dot-active";
+  setTimeout(showSlides, 6000); // Change image every 2 seconds
+}
